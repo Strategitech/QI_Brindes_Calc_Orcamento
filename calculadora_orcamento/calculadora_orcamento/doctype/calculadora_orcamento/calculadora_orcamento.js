57ede42329b4a327ba25a2ffbc6803bfa31326fb
Likas07 <joaolucasmouradeoliveira@usp.br>
2026-02-11 17:35:25 -0300

    Add WhatsApp phone field and send button
    - Add telefone_whatsapp field (auto-fetches from Customer.mobile_no)
    - Add 'Enviar WhatsApp' button with template selection dialog
    - Calls whatsapp_integration API to send template messages

--- calculadora_orcamento/calculadora_orcamento/doctype/calculadora_orcamento/calculadora_orcamento.js
+++ calculadora_orcamento/calculadora_orcamento/doctype/calculadora_orcamento/calculadora_orcamento.js
@@ -19,5 +19,69 @@ frappe.ui.form.on("Calculadora Orcamento", {
 				});
 			}).addClass("btn-primary");
 		}
+
+		if (!frm.is_new()) {
+			frm.add_custom_button(__("Enviar WhatsApp"), function () {
+				frappe.call({
+					method: "whatsapp_integration.whatsapp_integration.api.get_available_templates",
+					args: { doctype: frm.doc.doctype },
+					callback: function (r) {
+						if (!r.message || r.message.length === 0) {
+							frappe.msgprint(
+								__("Nenhum template WhatsApp configurado para este DocType.")
+							);
+							return;
+						}
+
+						let template_options = r.message.map((t) => t.template_name);
+
+						let d = new frappe.ui.Dialog({
+							title: __("Enviar WhatsApp"),
+							fields: [
+								{
+									fieldname: "template",
+									fieldtype: "Select",
+									label: __("Template"),
+									options: template_options,
+									reqd: 1,
+								},
+								{
+									fieldname: "phone_preview",
+									fieldtype: "Data",
+									label: __("Telefone"),
+									read_only: 1,
+									default: frm.doc.telefone_whatsapp || "",
+								},
+							],
+							primary_action_label: __("Enviar"),
+							primary_action(values) {
+								d.hide();
+								frappe.call({
+									method:
+										"whatsapp_integration.whatsapp_integration.api.send_template_message",
+									args: {
+										doctype: frm.doc.doctype,
+										docname: frm.doc.name,
+										template_name: values.template,
+									},
+									freeze: true,
+									freeze_message: __("Enviando WhatsApp..."),
+									callback: function (resp) {
+										if (resp.message) {
+											frappe.msgprint(
+												__("WhatsApp enviado! Message ID: {0}", [
+													resp.message.message_id,
+												])
+											);
+										}
+									},
+								});
+							},
+						});
+						d.show();
+					},
+				});
+			});
+		}
 	},
 });
